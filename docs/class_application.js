6baaf0e4f0102c47f2281d1e90503b5af6054159
aguiller31 <antoine.guillermin31@gmail.com>
2023-12-09 15:42:46 +0100

    docs

--- docs/class_application.js
+++
@@ -1,7 +0,0 @@
-var class_application =
-[
-    [ "Application", "class_application_afa8cc05ce6b6092be5ecdfdae44e05f8.html#afa8cc05ce6b6092be5ecdfdae44e05f8", null ],
-    [ "~Application", "class_application_a748bca84fefb9c12661cfaa2f623748d.html#a748bca84fefb9c12661cfaa2f623748d", null ],
-    [ "init", "class_application_a17d01f5d197b828322bd3c1b527d6701.html#a17d01f5d197b828322bd3c1b527d6701", null ],
-    [ "run", "class_application_aaf489cd96a237fb6b9b9c71d7791b3f5.html#aaf489cd96a237fb6b9b9c71d7791b3f5", null ]
-];
---
+++ html/de/d68/class_client_service.js
@@ -0,0 +1,7 @@
+var class_client_service =
+[
+    [ "ClientService", "de/d68/class_client_service_a79719a2bce5833e5a5372dbcfe089644.html#a79719a2bce5833e5a5372dbcfe089644", null ],
+    [ "~ClientService", "de/d68/class_client_service_a962e765ec5665356f32838c1055dd13e.html#a962e765ec5665356f32838c1055dd13e", null ],
+    [ "sendAngle", "de/d68/class_client_service_a26f6ce21394c9931e6d03ec3e05d4420.html#a26f6ce21394c9931e6d03ec3e05d4420", null ],
+    [ "setup", "de/d68/class_client_service_ab7e6159d6c3e536cba7d26a6df222527.html#ab7e6159d6c3e536cba7d26a6df222527", null ]
+];
